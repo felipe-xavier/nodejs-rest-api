948df29e75cfd89ad3c056a0c6f92da18f7d0267
felipe.xavier <felipe.xavier@funceme.br>
2020-10-21 20:05:17 -0300

    Create set-posts-query hook for before findingAll posts.

--- src/services/posts/posts.hooks.ts
+++ src/services/posts/posts.hooks.ts
@@ -1,6 +1,7 @@
 import * as authentication from '@feathersjs/authentication';
 import {disallow} from 'feathers-hooks-common';
 import setAuthorId from '../../hooks/set-author-id';
+import sortPostsQuery from '../../hooks/sort-posts-query';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 const { authenticate } = authentication.hooks;
@@ -8,7 +9,7 @@ const { authenticate } = authentication.hooks;
 export default {
   before: {
     all: [ authenticate('jwt') ],
-    find: [],
+    find: [sortPostsQuery()],
     get: [],
     create: [authenticate('jwt'), setAuthorId()],
     update: [disallow()],
