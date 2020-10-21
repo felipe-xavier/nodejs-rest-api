55a3153dd135494e0c289672cd2ec2c3ab10edae
felipe.xavier <felipe.xavier@funceme.br>
2020-10-21 16:30:59 -0300

    Create set-login hook.

---
+++ src/hooks/set-login.ts
@@ -0,0 +1,18 @@
+// Use this hook to manipulate incoming or outgoing data.
+// For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+import { Hook, HookContext } from '@feathersjs/feathers';
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default (options = {}): Hook => {
+  return async (context: HookContext): Promise<HookContext> => {
+    const { data } = context;
+
+    context.params.login = {
+      strategy: 'local',
+      username: data.username,
+      password: data.password
+    };
+
+    return context;
+  };
+};
