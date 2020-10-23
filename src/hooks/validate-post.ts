17151732c163b772b06609cadfd5c7fe6379b965
felipe.xavier <felipe.xavier@funceme.br>
2020-10-23 15:42:43 -0300

    Handle errors for users and posts validation.

--- src/hooks/validate-post.ts
+++ src/hooks/validate-post.ts
@@ -2,12 +2,16 @@
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
 import { Hook, HookContext } from '@feathersjs/feathers';
 import {postsSchema} from '../services/posts/posts.schema';
+import {BadRequest} from '@feathersjs/errors';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default (options = {}): Hook => {
   return async (context: HookContext): Promise<HookContext> => {
 
-    await postsSchema.validate(context.data);
+    await postsSchema.validate(context.data)
+      .catch(err => {
+        throw new BadRequest(err.errors[0]);
+      });
 
     return context;
   };
